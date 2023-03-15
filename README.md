1bbb849ca71d6e9d6d684849891db5fe95cf0702
Juan Velasco <juanayevela@yahoo.com>
2023-03-15 00:30:25 -0400

    enhanced QR code reader with multiple code scanning feature, added local storage and clickable hyperlinks

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # QR Code Reader
 
-A simple, user-friendly QR Code Reader web application built using the html5-qrcode library and JavaScript. This QR code reader allows users to scan QR codes using their device's camera and displays the decoded information on the screen.
+A feature-rich, user-friendly QR Code Reader web application built using the zxing-js/library and JavaScript. This web app allows users to efficiently scan QR codes using their device's camera, displaying the decoded information in a list on the screen, and storing the data in local storage for easy retrieval. With support for clickable hyperlinks, this QR code reader is perfect for both desktop and mobile devices.
 
 ## Features
 - Responsive design to work on various devices
-- User-friendly interface with clear instructions and a single button to start scanning
+- User-friendly interface with clear instructions and buttons to start and stop scanning
 - Uses the rear camera (if available) for scanning QR codes
 - Real-time scanning and decoding of QR codes
-- Displays the decoded QR code information on the screen
+- Displays the decoded QR code information on the screen in a list
+- Stores decoded QR codes in local storage with date information
+- Supports clickable hyperlinks when the decoded QR code contains a URL
 
 ## Installation and Usage
 1. Clone the repository or download the source code as a ZIP file.
 2. Open the index.html file in your preferred web browser.
 3. Click the "Start Scanning" button to initiate the QR code scanning process.
 4. Grant permission for the web application to access your device's camera.
-5. Point the camera towards a QR code to scan it. The decoded information will appear on the screen.
+5. Point the camera towards a QR code to scan it. The decoded information will appear on the screen in a list.
+6. Click the "Stop Scanning" button to stop the QR code scanning process.
 
 ## Technologies Used
 - HTML5
--- qr-reader.js
+++ qr-reader.js
@@ -2,10 +2,10 @@ const startButton = document.getElementById('start-button');
 const cameraStream = document.getElementById('camera-stream');
 const decodedQr = document.getElementById('decoded-qr');
 const stopButton = document.getElementById('stop-button');
-stopButton.addEventListener('click', stopScanning);
 let codeReader;
 
 startButton.addEventListener('click', startScanning);
+stopButton.addEventListener('click', stopScanning);
 
 async function startScanning() {
     startButton.style.display = 'none';
@@ -23,10 +23,25 @@ async function startScanning() {
 
     codeReader.decodeFromVideoDevice(cameraId, cameraStream, (result, err) => {
         if (result) {
-            decodedQr.innerText = result.text;
+            const decodedText = result.text;
+            const listItem = document.createElement('li');
+            listItem.textContent = decodedText;
+            listItem.dataset.date = new Date().toISOString();
+
+            if (isValidURL(decodedText)) {
+                const link = document.createElement('a');
+                link.href = decodedText;
+                link.textContent = decodedText;
+                listItem.textContent = '';
+                listItem.appendChild(link);
+            }
+
+            document.getElementById('decoded-qr-list').appendChild(listItem);
+            saveToLocalStorage(decodedText);
             stopButton.style.display = 'inline-block';
         }
     });
+
 
     } catch (err) {
         console.error(err);
@@ -37,14 +52,54 @@ async function startScanning() {
     }
 }
 
-function stopScanning() {
-    html5QrCode.stop().then(() => {
-        cameraStream.style.display = 'none';
-        stopButton.style.display = 'none';
-        startButton.style.display = 'inline-block'; // Add this line to show the "Start Scanning" button again
-    }).catch((error) => {
-        console.error("Error stopping the QR code scanning:", error);
-        alert("Error stopping the QR code scanning:\n" + error);
+function isValidURL(url) {
+    try {
+        new URL(url);
+        return true;
+    } catch (_) {
+        return false;
+    }
+}
+
+function saveToLocalStorage(decodedText) {
+    const storedData = JSON.parse(localStorage.getItem('qrCodes') || '[]');
+    const newItem = {
+        text: decodedText,
+        date: new Date().toISOString(),
+    };
+    storedData.push(newItem);
+    localStorage.setItem('qrCodes', JSON.stringify(storedData));
+}
+
+function loadFromLocalStorage() {
+    const storedData = JSON.parse(localStorage.getItem('qrCodes') || '[]');
+    storedData.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = item.text;
+        listItem.dataset.date = item.date;
+
+        if (isValidURL(item.text)) {
+            const link = document.createElement('a');
+            link.href = item.text;
+            link.textContent = item.text;
+            listItem.textContent = '';
+            listItem.appendChild(link);
+        }
+
+        document.getElementById('decoded-qr-list').appendChild(listItem);
     });
 }
 
+function stopScanning() {
+    try {
+        codeReader.reset(); // Use the reset method instead of stop
+        cameraStream.style.display = 'none';
+        stopButton.style.display = 'none';
+        startButton.style.display = 'inline-block';
+    } catch (error) {
+        console.error("Error stopping the QR code scanning:", error);
+        alert("Error stopping the QR code scanning:\n" + error);
+    }
+}
+
+loadFromLocalStorage();
